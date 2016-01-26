a924378cfbd239c788bd679c49adf61fef0116cc
roylee0704 <roylee0704@gmail.com>
2016-01-27 01:13:03 +0800

    updated

--- drinker.go
+++ drinker.go
@@ -1,73 +1,94 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
+// stateFn represents the state of an alcoholic as a function that returns the next state
 type stateFn func(d *drinker) stateFn
 
+// drinker holds the state of an alcoholic.
 type drinker struct {
 	money  int
 	cap    int
 	empty  int
 	bottle int
-	drink  int
+	drank  int
 	state  stateFn
 }
 
 func main() {
-	tan := Drinker(10)
-	fmt.Printf("%+v\n", tan.start())
+
+	var ringgit int
+
+	fmt.Print("Berapa Ringgit ada? (RM): ")
+	_, err := fmt.Scanf("%d", &ringgit)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "drinker: %v", err)
+	}
+
+	fmt.Printf("%+v\n", alcoholic(ringgit).start())
 }
 
-func Drinker(money int) *drinker {
+// alcohlic creates a new alcoholic, with money
+func alcoholic(money int) *drinker {
 	return &drinker{
 		money: money,
 	}
 }
 
+// start runs the state machine for the drinker.
 func (d *drinker) start() *drinker {
-	for d.state = Buy; d.state != nil; {
+	for d.state = buy; d.state != nil; {
 		d.state = d.state(d)
 	}
 
 	return d
 }
 
-func Drink(d *drinker) stateFn {
+// yamSheng to drink all of the available bottle.
+func yamSheng(d *drinker) stateFn {
 	d.cap += d.bottle
 	d.empty += d.bottle
-	d.drink += d.bottle
+	d.drank += d.bottle
 	d.bottle = 0
 
-	return Buy(d)
+	return buy(d)
 }
 
-func Buy(d *drinker) stateFn {
+// buy bottle with money, cap, empty bottle.
+func buy(d *drinker) stateFn {
 	switch {
 	case d.money%2 == 0 && d.money != 0:
-		return BuyWithMoney(d)
+		return buyWithMoney(d)
 	case d.cap >= 4 && d.cap != 0:
-		return BuyWithCap(d)
+		return buyWithCap(d)
 	case (d.empty >= 2) && d.empty != 0:
-		return BuyWithEmptyBottle(d)
+		return buyWithEmptyBottle(d)
 	default:
 		return nil
 	}
 }
 
-func BuyWithMoney(d *drinker) stateFn {
-	d.bottle += 1
+// buyWithMoney exchanges RM2 with 1 bottle.
+func buyWithMoney(d *drinker) stateFn {
 	d.money -= 2
-	return Drink(d)
+	d.bottle++
+	return yamSheng(d)
 }
 
-func BuyWithCap(d *drinker) stateFn {
-	d.bottle += 1
+// buyWithCap exchanges 4 caps with 1 bottle.
+func buyWithCap(d *drinker) stateFn {
 	d.cap -= 4
-	return Drink(d)
+	d.bottle++
+	return yamSheng(d)
 }
 
-func BuyWithEmptyBottle(d *drinker) stateFn {
-	d.bottle += 1
+// buyWithEmptyBottle exchanges 2 empty bottle with 1 bottle.
+func buyWithEmptyBottle(d *drinker) stateFn {
 	d.empty -= 2
-	return Drink(d)
+	d.bottle++
+	return yamSheng(d)
 }
